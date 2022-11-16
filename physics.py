ca2a4137faf4f04df6a011590cd8c4296d0ef66f
Sol 'Entropax' Maxwell <sol@entropax.ru>
2022-11-16 23:27:55 +0300

    add: poetry support, physics.py empty file with bare Physics class
    other changes: modify .gitignore for poetry

---
+++ physics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+physics module
+"""
+
+import random
+import pygame as pg
+
+
+class Physics():
+    '''
+    Game physics
+    '''
+    VERSION = 0.1
+
+    def __init__(self):
+        '''
+        Initialize our game with some additions
+        '''
+        return NotImplemented
